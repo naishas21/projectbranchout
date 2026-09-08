5506cecf2dbf0f2518f6f6fb24df1a103941936f
naishas21 <naisha.sing.21@gmail.com>
2026-09-07 19:11:53 -0700

    accordiannn

---
+++ script.js
@@ -0,0 +1,13 @@
+const questions = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < questions.length; i++) {
+  questions[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
